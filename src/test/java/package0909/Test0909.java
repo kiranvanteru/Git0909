70cf48498fa50c9b33cdddfc3c988cab913c80e7
kiranvanteru <kk23vanteru@gmail.com>
2018-09-09 21:48:08 +0530

    1st commit

---
+++ src/test/java/package0909/Test0909.java
@@ -0,0 +1,12 @@
+package package0909;
+
+import org.testng.annotations.Test;
+
+public class Test0909 {
+  @Test
+  public void Method0909() {
+	  System.out.println("0909 1st test");
+	  
+	  
+  }
+}
